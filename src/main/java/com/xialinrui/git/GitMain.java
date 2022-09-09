8b3d20d86b8d13277eb2534fa9d2f5f175d66a70
xialinrui <xialinrui@qq.com>
2022-09-09 14:25:50 +0800

    git commit master 3

--- src/main/java/com/xialinrui/git/GitMain.java
+++ src/main/java/com/xialinrui/git/GitMain.java
@@ -3,10 +3,14 @@ package com.xialinrui.git;
 public class GitMain {
 
     public static void main(String[] args) {
-
-
         System.out.println("Hello Git1!");
         System.out.println("Hello Git2!");
         System.out.println("Hello Git3!");
+        System.out.println("Hello Git3!");
+        System.out.println("Hello Git3!");
+        System.out.println("Hello Git3!");
+        System.out.println("Hello Git3!");
+        System.out.println("Hello Git3!");
+
     }
 }
